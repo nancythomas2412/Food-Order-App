6b47dbfe47f62bcd3c99f7c3218eb43208fa72ca
Nancy Thomas <nancytreasathomas@gmail.com>
2024-08-13 11:30:11 +0530

    Adds & Removes Cart Items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,33 +1,44 @@
 import { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
-import { currencyFormatter } from '../util/formatting.js';
-import Button from './UI/Button.jsx'
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./UI/CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
 
   function handleCloseCart() {
     userProgressCtx.hideCart();
   }
 
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
-          </ul>
+      </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
