1626ee6a5f81b50cf22544dcea26ab20a1e4b00f
Nancy Thomas <nancytreasathomas@gmail.com>
2024-08-16 15:21:27 +0530

    Adds Custom HTTP Hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-      if (!response.ok) {
-        //...
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
-    
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-          // <li key={meal.id}>{meal.name}</li>
-          <MealItem key={meal.id} meal={meal}/>
+        // <li key={meal.id}>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
